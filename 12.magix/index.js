84d03d6e0917826ab116ff3e4459cc6753f38b5c
Sharu24 <sharu.mukti@gmail.com>
2020-08-30 08:48:19 +0530

    updated 12

--- 12.magix/index.js
+++ 12.magix/index.js
@@ -1,36 +1,46 @@
-var rls = require("../node_modules/readline-sync");
-var magix, i, j, val;
+var rls = require("readline-sync");
 
-do {
-  (magix = []), (i = 0), (j = 0), (val = 0);
-  var mSize = rls.questionInt("Please enter the size of the matrix :");
-
-  var sum = Array(mSize * 2 + 2).fill(0);
-
-  var mCount = mSize * mSize;
-
-  for (var k = 1; k <= mCount; k++) {
-    // Every row reset iter and initialize magix
-    if (k % mSize === 1) {
+/* ********************************************************************* */
+function checkMagix(magix, mSize) {
+  var i = 0,
+    j = 0,
+    sum = Array(mSize * 2 + 2).fill(0);
+  count = mSize * mSize;
+  for (var x = 0; x < count; x++) {
+    if ((x + 1) % mSize === 1 || mSize === 1) {
       j = 0;
-      i = Math.floor(k / mSize);
-      magix[i] = new Array(2);
+      i = mSize === 1 ? 0 : Math.floor((x + 1) / mSize);
     }
-    val = rls.questionInt(`Please Enter Element #[${i}][${j}] :`);
-
-    magix[i][j] = val;
-
+    val = magix[i][j];
     sum[i] += val; // row
     sum[mSize + j] += val; // column
     if (i === j) sum[mSize * 2] += val; // lr diagonal
     if (i === mSize - 1 - j) sum[mSize * 2 + 1] += val; // rl diagonal
-
     j++;
   }
-  console.log(magix);
+  //   console.log(sum);
+  return sum.every(ele => ele === sum[0]) ? true : false;
+}
+
+/* ********************************************************************* */
+
+var magix, i, j, val;
+do {
+  (magix = []), (i = 0), (j = 0), (val = 0);
+  var mSize = rls.questionInt("Please enter the size of the matrix :");
+  var mCount = mSize * mSize;
+  for (var k = 1; k <= mCount; k++) {
+    if (k % mSize === 1 || mSize === 1) {
+      j = 0;
+      i = mSize === 1 ? 0 : Math.floor(k / mSize);
+      magix[i] = new Array(2);
+    }
+    val = rls.questionInt(`Please Enter the Element:`);
+    magix[i][j] = val;
+    j++;
+  }
+
   console.log(
-    sum.every(ele => ele === sum[0])
-      ? "Its a Magic Square"
-      : "Its not a Magix Square"
+    checkMagix(magix, mSize) ? "Its a Magic Square" : "Its not a Magix Square"
   );
 } while (rls.question("Wanna Try Again (y/n) ? : ") === "y" ? true : false);
