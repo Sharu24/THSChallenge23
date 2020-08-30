1b082ed0a3582f725d48fa64bcb09fe116f025bd
Sharu24 <sharu.mukti@gmail.com>
2020-08-30 09:50:41 +0530

    added function for Identity matrix

--- 14.Identity/index.js
+++ 14.Identity/index.js
@@ -1,35 +1,40 @@
 var rls = require("../node_modules/readline-sync");
-var i, j, flag, mSize, flag;
+var mSize,
+  mCount,
+  arr = [];
+
+function identity(arr) {
+  var x = 0,
+    len = arr.length,
+    flag = true;
+
+  for (var i = 0; i < len; i++) {
+    ele = arr[i];
+    if (!i || i === x + mSize + 1) {
+      x = i;
+      if (ele !== 1) flag = false;
+    } else if (ele) flag = false;
+    if (!flag) break;
+  }
+  return flag;
+}
 
 do {
-  i = j = 0;
-  flag = true;
-  mSize = rls.questionInt("Please enter the size of the Matrix : ");
+  arr = [];
+  console.log("Create a 'm' X 'm' matrix to check if its a Identity Matrix:");
+  mSize = rls.questionInt("Please enter the size of the Matrix 'm': ");
   mCount = mSize * mSize;
 
   do {
-    var ele = rls.questionInt(
-      `Enter the elements for element ${mSize * mSize - mCount + 1}/${mSize *
-        mSize}: `
+    arr.push(
+      rls.questionInt(
+        `Enter the elements for element ${mSize * mSize - mCount + 1}/${mSize *
+          mSize}: `
+      )
     );
-    if (!i || i === j + mSize + 1) {
-      j = i;
-      if (ele !== 1) flag = false;
-    } else if (ele) flag = false;
-    i++;
   } while (--mCount);
 
-  console.log(flag ? "Its an Identity Matrix" : "Its not an Identity Matrix");
+  console.log(
+    identity(arr) ? "Its an Identity Matrix" : "Its not an Identity Matrix"
+  );
 } while (rls.question("Wanna Try Again (y/n) ? : ") === "y" ? true : false);
-
-/*
-while (mCount) {
-  var ele = Number(rsl.question(`Enter the elements for ${i}st row: `));
-  if (i === 1 || i === j + mSize + 1) {
-    j = i;
-    if (ele !== 1) flag = false;
-  } else if (ele !== 0) flag = false;
-  i++;
-  mCount--;
-}
-*/
